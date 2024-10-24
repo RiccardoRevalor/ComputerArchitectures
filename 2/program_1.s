11c6fc3d8abedac0045e3eb5d776add3371d71f7
Silvia Bonenti <s295358@studenti.polito.it>
2024-10-24 16:29:56 +0200

    Upload lab1,2,3

--- 2/program_1.s
+++ 2/program_1.s
@@ -1,63 +1,40 @@
 .data
-v1: .double 1.0 2.0 3.0 4.0
-    .double 5.0 6.0 7.0 8.0
-    .double 9.0 10.0 11.0 12.0
-    .double 13.0 14.0 15.0 16.0
-    .double 17.0 18.0 19.0 20.0
-    .double 21.0 22.0 23.0 24.0
-    .double 25.0 26.0 27.0 28.0
-    .double 29.0 30.0 31.0 32.0
-
-v2: .double 2.0 2.0 3.0 4.0
-    .double 5.0 6.0 7.0 8.0
-    .double 9.0 10.0 11.0 12.0
-    .double 13.0 14.0 15.0 16.0
-    .double 17.0 18.0 19.0 20.0
-    .double 21.0 22.0 23.0 24.0
-    .double 25.0 26.0 27.0 28.0
-    .double 29.0 30.0 31.0 32.0
-
-v3: .double 1.0 2.0 3.0 4.0
-    .double 5.0 6.0 7.0 8.0
-    .double 9.0 10.0 11.0 12.0
-    .double 13.0 14.0 15.0 16.0
-    .double 17.0 18.0 19.0 20.0
-    .double 21.0 22.0 23.0 24.0
-    .double 25.0 26.0 27.0 28.0
-    .double 29.0 30.0 31.0 32.0
-
-v4: .space 256
-v5: .space 256
-v6: .space 256
+     v1: .double 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10
+          .double 11.11, 12.12, 13.13, 14.14, 15.15, 16.16, 17.17, 18.18
+          .double 19.19, 20.20, 21.21, 22.22, 23.23, 24.24, 25.25, 26.26
+          .double 27.27, 28.28, 29.29, 30.30, 31.31, 32.32
+     v2: .double 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0
+          .double 22.0, 24.0, 26.0, 28.0, 30.0, 32.0, 34.0, 36.0, 38.0
+          .double 40.0, 42.0, 44.0, 46.0, 48.0, 50.0, 52.0, 54.0, 56.0
+          .double 58.0, 60.0, 62.0, 64.0
+     v3: .double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1
+          .double 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2
+          .double 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2
+     v4: .space 256
+     v5: .space 256
+     v6: .space 256
 
 .text
+     daddui r1, r0, 248 # r1 holds the index i=31 (multiplied by 8)
+     daddi r2, r0, -8 # r2 holds the value to evaluate the loop condition 
+                         # (>= 0 corresponds to > -1, in my case -8 since its multiplied by 8)
+cycle:  l.d f1, v1(r1)
+     l.d f2, v2(r1)
+     #l.d f3, v3(r1) MOVED BELOW
+     mul.d f4, f1, f1
+     l.d f3, v3(r1)
+     sub.d f4, f4, f2
+     #s.d f4, v4(r1) MOVED BELOW
+     div.d f5, f4, f3
+     sub.d f6, f4, f1
+     sub.d f5, f5, f2
+     #s.d f5, v5(r1) MOVED BELOW
+     #sub.d f6, f4, f1 MOVED ABOVE
+     mul.d f6, f6, f5
+     s.d f4, v4(r1)
+     s.d f5, v5(r1)
+     s.d f6, v6(r1)
+     daddi r1, r1, -8
+     bne r1, r2, cycle
 
-    daddui r1, r0, 0     # r1 holds the index (i)
-    daddui r2, r0, 31    # r2 holds the loop counter (i = 31)
-
-cycle:
-    l.d f1, v1(r1)       
-    l.d f2, v2(r1)   
-    #l.d f3, v3(r1)   moved down       
-
-    mul.d f4, f1, f1 
-    l.d f3, v3(r1)  
-    sub.d f4, f4, f2    
-    s.d f4, v4(r1)       
-
-    div.d f5, f4, f3     
-    sub.d f5, f5, f2     
-    s.d f5, v5(r1)      
-
-    sub.d f6, f4, f1     
-    mul.d f6, f6, f5  
-    daddi r2, r2, -1   
-    slt r3, r2, r0 
-    s.d f6, v6(r1)      
-
-    daddui r1, r1, 8     
-    #daddi r2, r2, -1     moved up
-       
-    #slt r3, r2, r0       moved up
-    beq r3, r0, cycle   
-    halt                  
+halt
--- 3/program_1_b.s
+++ 3/program_1_b.s
@@ -1,123 +1,93 @@
 .data
-v1: .double 1.0 2.0 3.0 4.0
-    .double 5.0 6.0 7.0 8.0
-    .double 9.0 10.0 11.0 12.0
-    .double 13.0 14.0 15.0 16.0
-    .double 17.0 18.0 19.0 20.0
-    .double 21.0 22.0 23.0 24.0
-    .double 25.0 26.0 27.0 28.0
-    .double 29.0 30.0 31.0 32.0
-
-v2: .double 2.0 2.0 3.0 4.0
-    .double 5.0 6.0 7.0 8.0
-    .double 9.0 10.0 11.0 12.0
-    .double 13.0 14.0 15.0 16.0
-    .double 17.0 18.0 19.0 20.0
-    .double 21.0 22.0 23.0 24.0
-    .double 25.0 26.0 27.0 28.0
-    .double 29.0 30.0 31.0 32.0
-
-v3: .double 10.0 2.0 3.0 4.0
-    .double 5.0 6.0 7.0 8.0
-    .double 9.0 10.0 11.0 12.0
-    .double 13.0 14.0 15.0 16.0
-    .double 17.0 18.0 19.0 20.0
-    .double 21.0 22.0 23.0 24.0
-    .double 25.0 26.0 27.0 28.0
-    .double 29.0 30.0 31.0 32.0
-    .double 33.0 34.0 35.0 36.0
-
-v4: .space 256
-v5: .space 256
-v6: .space 256
-m: .word 1
-
+     
+     a: .space 8
+     b: .double 7.5
+     m: .word 1
+     v1: .double 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0
+          .double 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0
+          .double 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2
+     v2: .double 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0
+          .double 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8
+          .double 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6
+          .double 5.8, 6.0, 6.2, 6.4
+     v3: .double 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1
+          .double 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2
+          .double 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2
+     v4: .space 256
+     v5: .space 256
+     v6: .space 256
 
 .text
-    daddui r1, r0, 8     # r1 holds the index (i)
-    daddui r2, r0, 31    # r2 holds the loop counter (i = 31)
-    dmul r1, r1, r2      #r1 = 31 * 8
-    ld r3, m(r0)         #r3 = m = 1 at the beginning
-    daddui r8, r0, 1     #b = 1 (double)
-    #convert r8 to double
-    mtc1 r8, f8
-    daddui r7, r0, 2
-    daddui r14, r0, 3
-    cvt.d.l f8, f8
-    #r8 holds b
+     daddui  r3, r0, 3 # division by 3
+     daddui r1, r0, 248 # r1 holds the vectors index (i=31 multiplied by 8)
+     daddui r2, r0, 31 # r2 holds the index i=31
+     daddi r5, r0, -1 #check for loop exit condition
+     ld r6, m(r0) #load m into register
+     l.d f9, b(r0) #load b into register
 
-cycle:
-    #if int((i / 3)) * 3 != i, then i is not a multiple of 3 (Barrett Reduction)
-    #counter r7 goes from 0 to 2, if r7 == 0, then i is multiple of 3
-    l.d f2, v2(r1)
-    beq r7, r0, multiple_3
-    l.d f3, v3(r1)
-    j not_multiple_3
-    l.d f1, v1(r1)          
+cycle:  
 
-multiple_3: 
-    ##daddi r12, r12, 1 ##DEBUG
-    #perform: a = v1[i] / ((double)m<< i) /*logic shift */
-    dsllv r3, r3, r2    #m<<i
-    mtc1 r3, f4         #f4 contains m<<i in integer form
-    cvt.d.l f4, f4      #f4 contains m<<i in double form
-    div.d f5, f1, f4    #f5 contains a
+     # load vector values at index i
+     #l.d f1, v1(r1)
+     #l.d f2, v2(r1)
+     #l.d f3, v3(r1)
 
-    #m = int(a)
-    cvt.l.d f6, f5
+     #computation of i mod 3 in r4
+     ddivu r4, r2, r3   
+     l.d f1, v1(r1)
+     l.d f2, v2(r1)
+     #l.d f3, v3(r1)
+     dmulu r4, r4, r3
+     dsubu r4, r2, r4
+          
+     # check if branch condition and jump accordingly
+     beqz r4, multiple3
+     l.d f3, v3(r1)
+     #j no_multiple3
+    
+no_multiple3:
+     dmulu r6, r6, r2 #computer m*i
+     mtc1 r6, f7 #convert m*i to a double and store it in f7
+     cvt.d.l f7, f7
+     mul.d f8, f1, f7 #"a" computation
+     cvt.l.d f10, f8 
+     j end
+     mfc1 r6, f10 # m = int(a) conversion
 
-    #at the end: F5 = a, r3 = m
-    j end
-    mfc1 r3, f6         #in r3 there is m = int(a)
+multiple3:
+     dsllv r6, r6, r2 #shift m by i positions
+     mtc1 r6, f7 #convert shifted m to a double and store it in f7
+     cvt.d.l f7, f7
+     div.d f8, f1, f7 #"a" computation
+     cvt.l.d f10, f8 
+     mfc1 r6, f10 # m = int(a) conversion
+     #j end
 
-not_multiple_3:
-    ##daddi r11, r11, 1 ##DEBUG
-    #perform: a = v1[i] * ((double) m* i)
-    dmul r4, r3, r2     #m*i
-    mtc1 r4, f4         #f4 contains m*i in integer form
-    cvt.d.l f4, f4      #f4 contains m*i in double form
-    mul.d f5, f2, f4    #f5 contains a
-
-    #m = (int)a
-    cvt.l.d f6, f5
-    mfc1 r3, f6         #in r3 there is m = int(a)
-
-    #at the end: F5 = a, r3 = m
-
+# instructions after if condition inside for loop
 end:
+     #computation of v4[i]
+     mul.d f4, f8, f1
+     sub.d f4, f4, f2
+     #s.d f4, v4(r1)
+     
+     #computation of v5[i]
+     div.d f5, f4, f3
+     sub.d f6, f4, f1
+     sub.d f5, f5, f9
+     #s.d f5, v5(r1)
 
-#V4[i] block has to be executed here and in this order because V5[i] and V6[i] both depend on the value calculated here
-#perform: v4[i] = a*v1[i] – v2[i];
-    mul.d f6, f5, f1    #a*v1[i]
-    sub.d f6, f6, f2    #a*v1[i] - v2[i]
-    s.d f6, v4(r1)      #v4[i] = a*v1[i] - v2[i]
+     #computation of v6[i]
+     #sub.d f6, f4, f1
+     mul.d f6, f6, f5
+     s.d f4, v4(r1)
+     s.d f5, v5(r1)
+     s.d f6, v6(r1)
 
-#perform: v5[i] =v4[i]/v3[i] – b
-    div.d f7, f6, f3    #v4[i]/v3[i]
-    sub.d f9, f6, f1    #v4[i]-v1[i]
-    sub.d f7, f7, f8    #v4[i]/v3[i] - b
-    daddi r2, r2, -1    #r2 = r2 - 1
-    s.d f7, v5(r1)      #v5[i] = v4[i]/v3[i] - b
+     #update of indeces
+     #daddi r1, r1, -8
+     daddi r2, r2, -1
 
-#perform: v6[i] = (v4[i]-v1[i])*v5[i]
-    mul.d f9, f9, f7    #(v4[i]-v1[i])*v5[i]
-    daddui r7, r7, 1
-    s.d f9, v6(r1)      #v6[i] = (v4[i]-v1[i])*v5[i]
-
-#update indexes
-    bne r7, r14, end2
-    slt r10, r2, r0     #r10 = r2 < 0
-    #counter == 3, the reset counter
-    daddui r7, r0, 0
-
-end2:
-    beq r10, r0, cycle  #if r2 >= 0, go to cycle
-    daddi r1, r1, -8     #r1 = r1 - 8
-    halt
-
-
-
-
-
-
-
+     #check for loop branch condition and jump accordingly
+     bne r2, r5, cycle
+     daddi r1, r1, -8
+halt
